e5fe59187fe07c7419d7d53f888fbc93ebae1416
Dary Kusuma Hardian <dary.scaleocean@gmail.com>
2026-01-06 01:47:16 +0700

    bug

--- src/pages/home/Home.header.tsx
+++ src/pages/home/Home.header.tsx
@@ -1,24 +1,22 @@
 const HomeHeader: React.FC = () => {
   return (
-    <div className="shrink-0 w-screen h-screen snap-start bg-linear-to-bl from-sky-400 to-blue-950 flex items-center justify-center overflow-hidden relative">
+    <div className="shrink-0 w-screen h-screen snap-start bg-linear-to-bl from-sky-800 to-black flex items-center justify-center overflow-hidden relative">
       <p
         className="
         absolute
-
       select-none
       pointer-events-none
-      text-[5rem]
+      text-[2.5rem]
       font-light
-      uppercase
       tracking-tight
-      bg-linear-to-r from-white/60 to-white/4
+      bg-linear-to-r from-sky-50/4 to-sky-50/70
       bg-clip-text
       text-transparent
-      sm:text-[10rem]
+      sm:text-[5rem]
     "
       >
-        <span className="font-extrabold">KAY</span>
-        CORE
+        <span className="font-extrabold">DEPOK </span>
+        is the Best!!
       </p>
       <div className="absolute top-6 left-6">
         <p className="text-[1rem] font-extralight tracking-wide text-white">
@@ -26,13 +24,6 @@ const HomeHeader: React.FC = () => {
         </p>
       </div>
     </div>
-    // <div className="shrink-0 w-screen h-screen snap-start bg-linear-to-bl from-sky-500 to-black flex items-center justify-center relative">
-    //   <div className="absolute top-6 left-6 text-white">
-    //     <h1 className="text-[1rem] font-extralight tracking-wide">
-    //       <span className="font-bold">KAY</span> Digital Studio
-    //     </h1>
-    //   </div>
-    // </div>
   );
 };
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,30 @@ import { useBodyClass } from '../../hooks/useBodyClass';
 import HomeHeader from './Home.header';
 
 function Home() {
-  useBodyClass('bg-blue-50');
+  useBodyClass('bg-black');
 
   return (
     <div className="relative w-screen h-screen overflow-x-scroll flex snap-x snap-mandatory">
       <HomeHeader />
-      <div className="shrink-0 w-screen h-screen snap-start bg-sky-200 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">Section 2</h1>
+      <div className="relative overflow-hidden shrink-0 w-screen h-screen snap-start bg-linear-to-br from-sky-800 to-black flex items-center justify-center">
+        <p
+          className="
+        absolute
+      select-none
+      pointer-events-none
+      text-[3rem]
+      font-light
+      tracking-tight
+      bg-linear-to-r from-white/60 to-white/4
+      bg-clip-text
+      text-transparent
+      sm:text-[5rem]
+    "
+        >
+          <span className="font-extrabold">DEPOK </span>
+          so Cute!!
+        </p>
+        {/* <h1 className="text-4xl font-bold text-white">Section 2</h1> */}
       </div>
     </div>
   );
