e017b301ec9a62e07e50b054d575e49ba4ba36e5
Dary Kusuma Hardian <dary.scaleocean@gmail.com>
2026-01-09 02:55:36 +0700

    bug

--- src/pages/home/Hero/Header/Home.Hero.Header.Btn.tsx
+++ src/pages/home/Hero/Header/Home.Hero.Header.Btn.tsx
@@ -1,6 +1,6 @@
 const HomeHeroHeaderNavBtn: React.FC = () => {
   return (
-    <button className="btn-gradient rounded-full hover:text-midnight transition-colors cursor-pointer text-white py-4 px-6">
+    <button className="header-logo-cu btn-gradient rounded-full hover:text-midnight transition-colors cursor-pointer text-white py-4 px-6">
       Contact us
     </button>
   );
--- src/pages/home/Hero/Header/Home.Hero.Header.Nav.tsx
+++ src/pages/home/Hero/Header/Home.Hero.Header.Nav.tsx
@@ -3,8 +3,8 @@ import Logo from '../../../../assets/logo.png';
 const HomeHeroHeaderNav: React.FC = () => {
   return (
     <div className="flex justify-center items-center text-white text-[1rem] gap-17">
-      <div className="flex items-center gap-2 text-[1.25rem] font-light font-ibm">
-        <img src={Logo} className="w-[2.3rem]" />
+      <div className="flex items-center gap-2 text-[1.25rem] font-light font-ibm header-logo">
+        <img src={Logo} className="w-[2.3rem] header-logo-image" />
         <span className="font-bold">KAY</span> Digital Studio
       </div>
       <ul className="m2:flex hidden space-x-17">
