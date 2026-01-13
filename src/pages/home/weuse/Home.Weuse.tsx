62883c2dda1a31563f0bc9934287602f9c36fd87
Dary Kusuma Hardian <dary.scaleocean@gmail.com>
2026-01-14 03:57:05 +0700

    fixed

--- src/pages/home/weuse/Home.Weuse.tsx
+++ src/pages/home/weuse/Home.Weuse.tsx
@@ -1,17 +1,17 @@
 import blenderLogo from '../../../assets/brand/blender.png';
-import figmaLogo from '../../../assets/brand/figma.png';
-import flutterLogo from '../../../assets/brand/flutter.png';
+import figmaLogo from '../../../assets/brand/figma.webp';
+import flutterLogo from '../../../assets/brand/flutter.webp';
 import nestjsLogo from '../../../assets/brand/nestjs.png';
 import reactjsLogo from '../../../assets/brand/reactjs.png';
 import sketchupLogo from '../../../assets/brand/sketchup.png';
-import vscodeLogo from '../../../assets/brand/vscode.png';
-import postgreLogo from '../../../assets/brand/postgre.png';
-import dockerLogo from '../../../assets/brand/docker.png';
+import vscodeLogo from '../../../assets/brand/vscode.webp';
+import postgreLogo from '../../../assets/brand/postgre.webp';
+import dockerLogo from '../../../assets/brand/docker.webp';
 import tailwindLogo from '../../../assets/brand/tailwind.png';
 import getxLogo from '../../../assets/brand/getx.png';
 import viteLogo from '../../../assets/brand/vite.png';
-import prismaLogo from '../../../assets/brand/prisma.png';
-import nginxLogo from '../../../assets/brand/nginx.png';
+import prismaLogo from '../../../assets/brand/prisma.webp';
+import nginxLogo from '../../../assets/brand/nginx.webp';
 
 interface WeuseItems {
   src: string;
