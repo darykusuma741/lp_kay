e5beae474839b89e4accd4072c5ee183cbf139c4
Dary Kusuma Hardian <dary.scaleocean@gmail.com>
2026-01-19 01:10:30 +0700

    bug

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -7,7 +7,7 @@ function Services() {
       <div className="w-screen h-40 bg-amber-300">
         <div className="coba2"></div>
       </div>
-      <div className="flex w-screen bg-amber-200 overflow-scroll">
+      <div className="flex w-screen bg-amber-200 overflow-scroll justify-center">
         <div className="flex gap-1 coba3">
           <div className="coba3-item bg-blue-600"></div>
           <div className="coba3-item bg-blue-600"></div>
